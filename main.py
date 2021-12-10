493896918b8a78893112242169511e7518c635bb
Aperture32GLaDOS <66212752+Aperture32GLaDOS@users.noreply.github.com>
2021-12-10 20:45:39 +0000

    Added functionality for string conversion

--- main.py
+++ main.py
@@ -10,3 +10,11 @@ def owoToBytes(owoString: str) -> bytes:
     length = (len(binaryData) // 8) + 1  # Calculate the length of the resultant bytes object
     binaryData = int(binaryData, 2)  # Calculate the integer from the owos
     return binaryData.to_bytes(length, "big")  # Convert the integer into the original data
+
+def textToOwo(text: str) -> str:  # Turns regular text into owo-uwu binary
+    binaryData = text.encode("utf-8")
+    return bytesToOwo(binaryData)
+
+def owoToText(owo: str) -> str:  # Turns owo-uwu binary into regular text
+    binaryData = owoToBytes(owo)
+    return binaryData.decode("utf-8")
